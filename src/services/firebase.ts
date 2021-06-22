5191c518ca2031648157f0a53a7c931a6ab526c2
fernandoobarbosa <fernandobarbosa1697@gmail.com>
2021-06-22 12:30:14 -0300

    aula 02

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,7 +4,7 @@ import 'firebase/database'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_API_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
@@ -15,5 +15,7 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
 
-export const auth = firebase.auth()
-export const database = firebase.database()
+const auth = firebase.auth()
+const database = firebase.database()
+
+export { firebase, auth, database }
